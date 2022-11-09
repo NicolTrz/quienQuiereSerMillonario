691cac2881dec5fa8e9fe6d690223538cb244cbf
Nicol Trejos <104464329+NicolTrz@users.noreply.github.com>
2022-11-08 21:35:33 -0500

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,75 @@
+export const preguntas =[
+  {
+
+   0:{
+      title: "1. El reglamento del aprendiz se adopta como parte del acuerdo",
+
+      incorrecta1: " A. 009",
+      incorrecta2: "B. Entre el subdirector y los instructores",
+      response: "C. 007",
+      incorrecta3: "D. Entre la comunidad educativa",
+    },
+
+    1:{
+      title: "2. De acuerdo el reglamento son derechos del aprendiz Sena.",
+
+      incorrecta1: "Presentar los instrumentos de evaluación cuando se sienta preparado.",
+      incorrecta2: "Tener conocimiento de los resultados obtenidos en las evaluaciones dentro d ellos 8 días hábiles siguientes.",
+      incorrecta3: "Recibir formación únicamente en la parte técnica.",
+      response: "Certificarse únicamente por contrato de aprendizaje o pasantía."
+    },
+
+    2:{
+      title: "3. Según el capitulo II articulo 8 del reglamento del aprendiz, los estímulos e incentivos que forman parte plan nacional de bienestar son:",
+
+      incorrecta1: "Recibir auxilio de transporte",
+      incorrecta2: "Reconocimiento por parte del comité de evaluación a los aprendices que se destacan por su excelente desempeño académico y convivencia.",
+      response: "Ser exonerado de la presentación de instrumentos de evaluación",
+      incorrecta3: "Recibir apoyo de alimentación (almuerzos)"
+    },
+
+    3:{
+      title: "4. El reglamento habla de los deberes de los aprendices en el capítulo.",
+
+      incorrecta1: "VII",
+      response: "VI",
+      incorrecta2: "II",
+      incorrecta3: "III"
+    },
+
+    4:{
+      title: "5. De acuerdo con el reglamento son deberes de los aprendices:",
+
+      response: "Cumplir con las actividades de su proceso formativo durante la etapa lectiva",
+      incorrecta1: "Conocer y asumir directrices institucionales, al igual que el reglamento del aprendiz, y cumplir las que sean de su agrado.",
+      incorrecta2: "Portar permanentemente el carné en un lugar visible, durante su proceso formativo, y renovarlo de acuerdo con las disposiciones vigente y devolverlo al finalizar el programa.",
+      incorrecta3: "Postularse a las convocatorias de apoyos socioeconómicos para tener como subsidiar gastos de la formación."
+    },
+
+    5:{
+      title: "6. Quienes son los encargados de seleccionar los candidatos a beneficiarios de los estímulos e incentivos de conformidad con los criterios estipulados en el reglamento.",
+
+      incorrecta1: "Los directores de grupo",
+      response: "Los coordinadores académicos",
+      incorrecta2: "Los voceros",
+      incorrecta3: "El comité de evaluación y seguimiento"
+    },
+
+    6:{
+      title: "7. De acuerdo con el capítulo IV donde se establecen las prohibiciones para los aprendices están:",
+
+      incorrecta1: "Terminar unilateralmente el contrato de aprendizaje, sin el visto bueno del empleador y del Sena.",
+      incorrecta2: "Manifestarles a los instructores y administrativos sus puntos de vista o inconformidades.",
+      incorrecta3: "Fumar o consumir sustancias alcohólicas o psicoactivas en los ambientes de formación.",
+      response: "Fumar o consumir sustancias alcohólicas o psicoactivas en los ambientes de formación."
+    },
+
+    7:{
+      title: "8. Es deber de los aprendices:",
+      
+      incorrecta1: "Portar el uniforme el fin de semana cuando se reúna con los compañeros del Sena.",
+      response: "Portar el carné cuando asista a conciertos, discotecas, restaurantes, fiestas de 15, grados y además actividades sociales, como representante de la comunidad Sena.",
+      incorrecta2: "Respetar los bienes y elementos de propiedad de los integrantes de la comunidad que estén en su ambiente de formación.",
+      incorrecta3: "Utilizar la dotación o ropa de trabajo y elementos de protección personal."
+    },
+}];
